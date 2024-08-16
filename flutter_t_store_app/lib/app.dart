27c1592bd41a420e5c2fe814520916333647a4f1
STPLSACHIN <126646261+STPLSACHIN@users.noreply.github.com>
2024-08-16 18:55:17 +0530

    some constant file added

--- flutter_t_store_app/lib/app.dart
+++ flutter_t_store_app/lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_t_store_app/utils/theme/theme.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
